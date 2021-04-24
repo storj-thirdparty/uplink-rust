8cdec0e0d603e4cec6b3e375f6c37f66a1272502
Cameron Fyfe <cameron.j.fyfe@gmail.com>
2021-04-23 22:01:32 -0600

    rustfmt

--- uplink-sys/src/lib.rs
+++ uplink-sys/src/lib.rs
@@ -1,5 +1,5 @@
 //! This crate provides Rust bindings to [uplink-c](https://github.com/storj/uplink-c/), the C interface for the storj uplink API library.
-//! 
+//!
 //! [TODO]() is the safe wrapper crate for this library.
 
 // Ignore style warnings for for uplink-c bindings
