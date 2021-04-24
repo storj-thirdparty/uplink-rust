6f631e707ee855f3d626ed22bc89adbc93bbbb86
Cameron Fyfe <cameron.j.fyfe@gmail.com>
2021-04-23 21:24:39 -0600

    add short description to docs

--- uplink-sys/src/lib.rs
+++ uplink-sys/src/lib.rs
@@ -1,3 +1,7 @@
+//! This crate provides Rust bindings to [uplink-c](https://github.com/storj/uplink-c/), the C interface for the storj uplink API library.
+//! 
+//! [TODO]() is the safe wrapper crate for this library.
+
 // Ignore style warnings for for uplink-c bindings
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
